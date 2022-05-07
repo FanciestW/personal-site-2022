1ce9573be5e4a5cbc6057962e1c18c913ceda332
William Lin <wlin26@yahoo.com>
2022-05-07 12:29:57 -0400

    Initialized gatsby site

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `personal-site-2022`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
