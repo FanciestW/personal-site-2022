e8f2669cb82313be536edc203fcec73169f15fe4
William Lin <wlin26@yahoo.com>
2022-07-04 10:42:20 -0400

    WIP:Getting mix-blend working on wave background image

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -23,7 +23,7 @@ const IntroCard = () => {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center gap-y-8 w-full h-full sm:w-50">
+    <div className="absolute flex flex-col items-center justify-center gap-y-8 w-full h-full sm:w-50">
       <h1 className="font-sans font-light text-light-purple text-5xl self-stretch leading-10 text-center items-center text-shadow shadow-black/25">
         Hello, I'm William
       </h1>
@@ -37,7 +37,7 @@ const IntroCard = () => {
             faIcon={faIcon}
             url={url}
             target="_blank"
-            className="text-6xl text-deep-purple transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl"
+            className="text-6xl mix-blend-overlay text-light-purple transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl"
           />
         ))}
       </div>
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,11 +4,14 @@ import introWaveSvg from "../assets/intro_wave.svg";
 
 const MainPage = () => {
   return (
-    <div className="bg-deep-purple">
+    <div>
+    <div className="mix-blend-overlay isolate bg-deep-purple bg-intro-wave-svg bg-no-repeat bg-relative bg-[right_0_bottom_0] bg-contain">
       <div className="flex h-fit w-full min-h-screen min-w-full items-center justify-center">
-        <img src={introWaveSvg} className="absolute z-0 w-screen bg-transparent bottom-[20vh]" />
+        {/* <img src={introWaveSvg} className="absolute z-0 w-screen bg-transparent bottom-0" /> */}
         <IntroCard />
       </div>
+    </div>
+    <div className="bg-light-purple h-screen w-full"></div>
     </div>
   );
 };
