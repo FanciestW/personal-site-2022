d6c51634786eba675571863762ba0525148178af
William Lin <wlin26@yahoo.com>
2022-07-11 19:31:50 -0400

    Move intro card back to component with mix-blend

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -24,20 +24,20 @@ const IntroCard = () => {
   ];
   return (
     <div className="absolute flex flex-col items-center justify-center gap-y-8 w-full h-full sm:w-50">
-      <h1 className="font-sans font-light text-light-purple text-5xl self-stretch leading-10 text-center items-center text-shadow shadow-black/25">
+      <h1 className="font-sans font-light text-blended-text mix-blend-difference text-5xl self-stretch leading-10 text-center items-center text-shadow shadow-black/25">
         Hello, I'm William
       </h1>
       <img
         className="overflow-visible w-2/5 min-w-[300px] max-w-[400px] rounded-full border-2 border-white drop-shadow-xl"
         src={profileImage}
       />
-      <div className="flex flex-row items-center justify-center gap-x-8 w-fit z-10">
+      <div className="flex flex-row items-center justify-center gap-x-8 w-fit z-10 text-blended-text mix-blend-difference">
         {LinkIconMapping.map(({ faIcon, url }) => (
           <LinkIcon
             faIcon={faIcon}
             url={url}
             target="_blank"
-            className="text-6xl text-blended-text mix-blend-difference transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl"
+            className="text-6xl transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl visited:text-blended-text focus:outline-none focus-visible:outline-none"
           />
         ))}
       </div>
