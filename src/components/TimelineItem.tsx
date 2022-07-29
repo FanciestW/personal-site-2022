d4fe72e25f76225051564cd9c590d2b298601a98
William Lin <wlin26@yahoo.com>
2022-07-28 22:57:26 -0400

    Updated style

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -26,7 +26,7 @@ interface IData {
 
 const TimelineItem = ({ data }: IData) => (
   <div className="timeline-item flex justify-end my-2.5 pr-8 relative w-6/12">
-    <div className="timeline-item-content">
+    <div className="timeline-item-content text-light-purple">
       <span className="tag" style={{ background: data.category.color }}>
         {data.category.tag}
       </span>
