ddf2d80eae755c0fc298a7911283218215af39e1
William Lin <wlin26@yahoo.com>
2022-08-01 20:54:28 -0400

    Added contact page information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,37 @@ import NavBar from "../components/NavBar";
 import AboutMe from "../components/AboutMe";
 import Timeline from "../components/Timeline";
 
+import {
+  faPhone, faAt,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin, faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const IndexPage = () => {
+  const contactInfoList = [
+    {
+      icon: faPhone,
+      text: "(917) 434-8023",
+      link: "tel:9174348023",
+    },
+    {
+      icon: faAt,
+      text: "WLIN26@YAHOO.COM",
+      link: "mailto:wlin26@yahoo.com",
+    },
+    {
+      icon: faLinkedin,
+      text: "https://linkedin.com/in/FanciestW",
+      link: "https://linkedin.com/in/FanciestW",
+    },
+    {
+      icon: faGithub,
+      text: "https://github.com/FanciestW",
+      link: "https://github.com/FanciestW",
+    },
+  ]
   return (
     <div className="select-none">
       <NavBar />
@@ -35,8 +65,24 @@ const IndexPage = () => {
       </div>
       <div
         id="contact"
-        className="snap-start snap-always flex justify-center lg:items-center bg-light-purple h-fit min-h-screen w-full bg-wave-3-svg bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
+        className="snap-start snap-always flex flex-col flex-start gap-12 items-center bg-light-purple h-fit min-h-screen w-full  bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
       >
+        <img src="/assets/wave_3.svg" className="w-full"></img>
+        <div className="flex flex-col sm:flex-row justify-center flex-nowrap gap-12 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
+          <div className="whitespace-nowrap font-sans text-3xl font-normal uppercase">
+            Get In Touch
+          </div>
+          <div className="flex flex-col gap-4 shrink font-san font-thin text-lg md:text-xl lg:text-2xl">
+            { contactInfoList.map((contactInfo, key) => (
+              <div className="flex flex-row gap-4 items-center">
+                <FontAwesomeIcon icon={contactInfo.icon} />
+                <a href={contactInfo.link} target="_blank">
+                  <span>{contactInfo.text}</span>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
