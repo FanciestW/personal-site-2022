932f8dcce53ac84bf383e418571606da660f8b83
William Lin <wlin26@yahoo.com>
2022-08-01 21:11:48 -0400

    Updated contact page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,16 +68,17 @@ const IndexPage = () => {
         className="snap-start snap-always flex flex-col flex-start gap-12 items-center bg-light-purple h-fit min-h-screen w-full  bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
       >
         <img src="/assets/wave_3.svg" className="w-full"></img>
-        <div className="flex flex-col sm:flex-row justify-center flex-nowrap gap-12 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
+        <div className="flex flex-col md:flex-row justify-center flex-nowrap gap-8 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
           <div className="whitespace-nowrap font-sans text-3xl font-normal uppercase">
             Get In Touch
           </div>
+          <div className="hidden md:block h-[150px] border-l-2 border-solid border-blended-text"></div>
           <div className="flex flex-col gap-4 shrink font-san font-thin text-lg md:text-xl lg:text-2xl">
-            { contactInfoList.map((contactInfo, key) => (
+            { contactInfoList.map((contactInfo) => (
               <div className="flex flex-row gap-4 items-center">
-                <FontAwesomeIcon icon={contactInfo.icon} />
+                <FontAwesomeIcon icon={contactInfo.icon} className="text-3xl"/>
                 <a href={contactInfo.link} target="_blank">
-                  <span>{contactInfo.text}</span>
+                  <span className="text-md sm:text-md lg:text-xl">{contactInfo.text}</span>
                 </a>
               </div>
             ))}
