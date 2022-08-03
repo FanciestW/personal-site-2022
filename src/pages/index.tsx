e611d37396ddffff8727b879569f81a8a4c8ff47
William Lin <wlin26@yahoo.com>
2022-08-02 20:10:45 -0400

    Centered contact info on rest of page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,8 @@ import NavBar from "../components/NavBar";
 import AboutMe from "../components/AboutMe";
 import Timeline from "../components/Timeline";
 
-import {
-  faPhone, faAt,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faLinkedin, faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faAt } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const IndexPage = () => {
@@ -34,7 +30,7 @@ const IndexPage = () => {
       text: "https://github.com/FanciestW",
       link: "https://github.com/FanciestW",
     },
-  ]
+  ];
   return (
     <div className="select-none">
       <NavBar />
@@ -68,18 +64,19 @@ const IndexPage = () => {
         className="snap-start snap-always flex flex-col flex-start gap-12 items-center bg-light-purple h-fit min-h-screen w-full  bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
       >
         <img src="/assets/wave_3.svg" className="w-full"></img>
-        {/* TODO::Grow the content below so that its center in the rest of the contact page. */}
-        <div className="flex flex-col md:flex-row justify-center flex-nowrap gap-8 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
+        <div className="flex flex-col md:flex-row grow justify-center flex-nowrap gap-8 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
           <div className="whitespace-nowrap font-sans text-3xl font-normal uppercase">
             Get In Touch
           </div>
           <div className="hidden md:block h-[150px] border-l-2 border-solid border-blended-text"></div>
           <div className="flex flex-col gap-4 shrink font-san font-thin text-lg md:text-xl lg:text-2xl">
-            { contactInfoList.map((contactInfo) => (
+            {contactInfoList.map((contactInfo) => (
               <div className="flex flex-row gap-4 items-center">
-                <FontAwesomeIcon icon={contactInfo.icon} className="text-3xl"/>
+                <FontAwesomeIcon icon={contactInfo.icon} className="text-3xl" />
                 <a href={contactInfo.link} target="_blank">
-                  <span className="text-md sm:text-md lg:text-xl">{contactInfo.text}</span>
+                  <span className="text-md sm:text-md lg:text-xl">
+                    {contactInfo.text}
+                  </span>
                 </a>
               </div>
             ))}
