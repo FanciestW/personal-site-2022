f97042a022e8e86662e49f8a20e2f58ed6d09a81
William Lin <wlin26@yahoo.com>
2022-08-05 11:01:56 -0400

    Formatted code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,25 +5,39 @@ const NavBar = () => {
   return (
     <div className="container fixed top-0 right-0 z-50 mix-blend-difference hidden sm:block">
       <div className="flex flex-row justify-end gap-x-12 px-10 py-8 z-0 uppercase text-base text-blended-text">
-        <Link spy hashSpy activeClass="underline underline-offset-8" className="hover:cursor-pointer" to="home">
+        <Link
+          spy
+          hashSpy
+          activeClass="underline underline-offset-8"
+          className="hover:cursor-pointer"
+          to="home"
+        >
           Home
         </Link>
         <Link
           spy
           hashSpy
           activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer" to="about-me"
+          className="hover:cursor-pointer"
+          to="about-me"
         >
           About Me
         </Link>
-        <Link spy hashSpy activeClass="underline underline-offset-8" className="hover:cursor-pointer" to="work">
+        <Link
+          spy
+          hashSpy
+          activeClass="underline underline-offset-8"
+          className="hover:cursor-pointer"
+          to="work"
+        >
           Work History
         </Link>
         <Link
           spy
           hashSpy
           activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer" to="contact"
+          className="hover:cursor-pointer"
+          to="contact"
         >
           Contact Info
         </Link>
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,55 +7,64 @@ const timelineData = [
     title: "Capital One",
     subtitle: "Software Engineer",
     date: "(February 2020 - present)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "University of New Haven",
     subtitle: "Graduate Computer Science Teaching Assistant",
     date: "(September 2019 - December 2020)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "Capital One",
     subtitle: "Software Engineer Intern",
     date: "(June 2020 - August 2020)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "SoluTech Inc.",
     subtitle: "Lead Software Engineer",
     date: "(January 2017 - October 2019)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "Capital One",
     subtitle: "Software Engineer Intern",
     date: "(June 2019 - August 2019)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "Conferdense Inc.",
     subtitle: "Android Developer",
     date: "(March 2018 - May 2019)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "The MITRE Corporation",
     subtitle: "Software Engineering Intern",
     date: "(June 2018 - August 2018)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "Abbott Jewelry Systems Inc.",
     subtitle: "Software Engineer",
     date: "(October 2013 - May 2018)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
   {
     title: "Self Employed",
     subtitle: "IT Technician",
     date: "(August 2015 - August 2017)",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget in odio arcu condimentum risus Mattis est sem eget mattis tortor risus quis. Est quis facilisis sit purus, quis elementum vitae non a.",
   },
 ];
 
