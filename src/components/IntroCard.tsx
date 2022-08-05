e29f3e1ef867876ea5f1fcfe5a0f415c38257968
William Lin <wlin26@yahoo.com>
2022-08-05 10:56:10 -0400

    Isolated component code

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -23,23 +23,30 @@ const IntroCard = () => {
     },
   ];
   return (
-    <div className="absolute flex flex-col items-center justify-center gap-y-8 w-full h-full sm:w-50">
-      <h1 className="font-sans font-light text-blended-text mix-blend-difference text-5xl self-stretch leading-10 text-center items-center text-shadow shadow-black/25">
-        Hello, I'm William
-      </h1>
-      <img
-        className="overflow-visible w-2/5 min-w-[300px] max-w-[400px] rounded-full border-2 border-white drop-shadow-xl"
-        src={profileImage}
-      />
-      <div className="flex flex-row items-center justify-center gap-x-8 w-fit z-10 text-blended-text mix-blend-difference">
-        {LinkIconMapping.map(({ faIcon, url }) => (
-          <LinkIcon
-            faIcon={faIcon}
-            url={url}
-            target="_blank"
-            className="text-6xl transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl visited:text-blended-text focus:outline-none focus-visible:outline-none"
+    <div
+      id="home"
+      className="snap-start snap-always bg-deep-purple bg-wave-1-svg bg-no-repeat bg-relative bg-[right_0_bottom_0] bg-contain"
+    >
+      <div className="flex h-fit w-full min-h-screen min-w-full items-center justify-center">
+        <div className="absolute flex flex-col items-center justify-center gap-y-8 w-full h-full sm:w-50">
+          <h1 className="font-sans font-light text-blended-text mix-blend-difference text-5xl self-stretch leading-10 text-center items-center text-shadow shadow-black/25">
+            Hello, I'm William
+          </h1>
+          <img
+            className="overflow-visible w-2/5 min-w-[300px] max-w-[400px] rounded-full border-2 border-white drop-shadow-xl"
+            src={profileImage}
           />
-        ))}
+          <div className="flex flex-row items-center justify-center gap-x-8 w-fit z-10 text-blended-text mix-blend-difference">
+            {LinkIconMapping.map(({ faIcon, url }) => (
+              <LinkIcon
+                faIcon={faIcon}
+                url={url}
+                target="_blank"
+                className="text-6xl transition ease-in-out duration-300 hover:scale-110 hover:drop-shadow-xl visited:text-blended-text focus:outline-none focus-visible:outline-none"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,51 +3,14 @@ import IntroCard from "../components/IntroCard";
 import NavBar from "../components/NavBar";
 import AboutMe from "../components/AboutMe";
 import Timeline from "../components/Timeline";
-
-import { faPhone, faAt } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ContactInfo from "../components/ContactInfo";
 
 const IndexPage = () => {
-  const contactInfoList = [
-    {
-      icon: faPhone,
-      text: "(917) 434-8023",
-      link: "tel:9174348023",
-    },
-    {
-      icon: faAt,
-      text: "WLIN26@YAHOO.COM",
-      link: "mailto:wlin26@yahoo.com",
-    },
-    {
-      icon: faLinkedin,
-      text: "https://linkedin.com/in/FanciestW",
-      link: "https://linkedin.com/in/FanciestW",
-    },
-    {
-      icon: faGithub,
-      text: "https://github.com/FanciestW",
-      link: "https://github.com/FanciestW",
-    },
-  ];
   return (
     <div className="select-none">
       <NavBar />
-      <div
-        id="home"
-        className="snap-start snap-always bg-deep-purple bg-wave-1-svg bg-no-repeat bg-relative bg-[right_0_bottom_0] bg-contain"
-      >
-        <div className="flex h-fit w-full min-h-screen min-w-full items-center justify-center">
-          <IntroCard />
-        </div>
-      </div>
-      <div
-        id="about-me"
-        className="snap-start snap-always flex justify-center lg:items-center bg-light-purple h-fit min-h-screen w-full"
-      >
-        <AboutMe />
-      </div>
+      <IntroCard />
+      <AboutMe />
       <div
         id="work"
         className="snap-start snap-always flex justify-center lg:items-center bg-deep-purple h-fit min-h-screen w-full bg-wave-2-svg bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
@@ -59,30 +22,7 @@ const IndexPage = () => {
           <Timeline />
         </div>
       </div>
-      <div
-        id="contact"
-        className="snap-start snap-always flex flex-col flex-start gap-12 items-center bg-light-purple h-fit min-h-screen w-full  bg-no-repeat bg-relative bg-[right_0_top_0] bg-contain"
-      >
-        <img src="/assets/wave_3.svg" className="w-full"></img>
-        <div className="flex flex-col md:flex-row grow justify-center flex-nowrap gap-8 min-h-fit w-9/12 max-w-screen-lg items-center mix-blend-difference text-blended-text py-10 sm:py-0">
-          <div className="whitespace-nowrap font-sans text-3xl font-normal uppercase">
-            Get In Touch
-          </div>
-          <div className="hidden md:block h-[150px] border-l-2 border-solid border-blended-text"></div>
-          <div className="flex flex-col gap-4 shrink font-san font-thin text-lg md:text-xl lg:text-2xl">
-            {contactInfoList.map((contactInfo) => (
-              <div className="flex flex-row gap-4 items-center">
-                <FontAwesomeIcon icon={contactInfo.icon} className="text-3xl" />
-                <a href={contactInfo.link} target="_blank">
-                  <span className="text-md sm:text-md lg:text-xl">
-                    {contactInfo.text}
-                  </span>
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <ContactInfo />
     </div>
   );
 };
