38b1706930a9ae4a2b29ae3da4c47700c95a5045
William Lin <wlin26@yahoo.com>
2022-08-30 21:08:09 -0400

    Fixed old code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,28 +6,6 @@ import WorkHistory from "../components/WorkHistory";
 import ContactInfo from "../components/ContactInfo";
 
 const IndexPage = () => {
-  const contactInfoList = [
-    {
-      icon: faPhone,
-      text: "(917) 434-8023",
-      link: "tel:9174348023",
-    },
-    {
-      icon: faAt,
-      text: "WLIN26@YAHOO.COM",
-      link: "mailto:wlin26@yahoo.com",
-    },
-    {
-      icon: faLinkedin,
-      text: "https://linkedin.com/in/FanciestW",
-      link: "https://linkedin.com/in/FanciestW",
-    },
-    {
-      icon: faGithub,
-      text: "https://github.com/FanciestW",
-      link: "https://github.com/FanciestW",
-    },
-  ];
   return (
     <div className="select-none">
       <NavBar />
