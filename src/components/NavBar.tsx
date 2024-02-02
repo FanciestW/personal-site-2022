c39e30b58b42a9693ab262d5cd7624193c668385
William Lin <wlin26@yahoo.com>
2024-02-02 13:23:41 -0500

    WIP:Adding mobile nav menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,48 +1,52 @@
 import * as React from "react";
 import { Link } from "react-scroll";
 
+interface ILink {
+  href: string;
+  text: string;
+}
+
+const links = [
+  {
+    href: "home",
+    text: "Home",
+  },
+  {
+    href: "about-me",
+    text: "About Me",
+  },
+  {
+    href: "work",
+    text: "Work History",
+  },
+  {
+    href: "contact",
+    text: "Contact Info",
+  },
+];
+
 const NavBar = () => {
   return (
-    <div className="container fixed top-0 right-0 z-50 mix-blend-difference hidden sm:block">
-      <div className="flex flex-row justify-end gap-x-12 px-10 py-8 z-0 uppercase text-base text-blended-text">
-        <Link
-          spy
-          hashSpy
-          activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer"
-          to="home"
-        >
-          Home
-        </Link>
-        <Link
-          spy
-          hashSpy
-          activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer"
-          to="about-me"
-        >
-          About Me
-        </Link>
-        <Link
-          spy
-          hashSpy
-          activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer"
-          to="work"
-        >
-          Work History
-        </Link>
-        <Link
-          spy
-          hashSpy
-          activeClass="underline underline-offset-8"
-          className="hover:cursor-pointer"
-          to="contact"
-        >
-          Contact Info
-        </Link>
+    <>
+      <div className="container fixed top-0 right-0 z-50 mix-blend-difference hidden sm:block">
+        <div className="flex flex-row justify-end gap-x-12 px-10 py-8 z-0 uppercase text-base text-blended-text">
+          {links.map(({ href, text }: ILink) => (
+            <Link
+              spy
+              hashSpy
+              activeClass="underline underline-offset-8"
+              className="hover:cursor-pointer"
+              to={href}
+            >
+              {text}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="container fixed top-0 right-0 z-50 w-full mix-blend-difference block sm:hidden">
+        Mobile Nav Menu
+      </div>
+    </>
   );
 };
 
